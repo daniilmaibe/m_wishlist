ae9718c425b7e327062fdb2230fcbb11ef706462
Daniil_Maibe <daniilmaibe@gmail.com>
2025-02-05 21:45:48 +0300

    v2

--- create.php
+++ create.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Проверяем, существует ли уже такой вишлист
     $wishlist_data_file = "wishlists/{$wishlist_id}_data.txt";
     if (file_exists($wishlist_data_file)) {
-        echo "Вишлист с таким идентификатором уже существует!";
+        echo "<h2>Вишлист с таким идентификатором уже существует! <a href='index.php'>Вернуться</a></h2> ";
     } else {
         // Создаем данные вишлиста
         $wishlist_data = [
@@ -18,7 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         // Сохраняем данные в файл
         file_put_contents($wishlist_data_file, json_encode($wishlist_data));
-        echo "Вишлист успешно создан! Перейдите по ссылке: <a href='wishlist.php?id={$wishlist_id}'>http://localhost/m_wishlist/wishlist.php?id={$wishlist_id}</a>";
+        echo "<h2> Вишлист успешно создан! Перейдите по ссылке: <a href='wishlist.php?id={$wishlist_id}'>http://localhost/m_wishlist/wishlist.php?id={$wishlist_id}</a></h2>";
     }
 }
 ?>
